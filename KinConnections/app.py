3a4f483e998c4bb8bffc6d0486906c3e9918a45d
Shanil Jasani <shaniljasani@utexas.edu>
2021-05-06 13:08:34 -0500

    Trailing slash fix

--- KinConnections/app.py
+++ KinConnections/app.py
@@ -2,6 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_url_path="/static")
 
+# redirect on trailing slashes
+@app.before_request
+def clear_trailing():
+    from flask import redirect, request
+
+    rp = request.path 
+    if rp != '/' and rp.endswith('/'):
+        return redirect(rp[:-1])
+
 # the home/main page
 @app.route('/')
 def index():
