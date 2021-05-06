0f5424c7f4598d5c9e1b87a8e2b3cd14682c6239
Shanil Jasani <shaniljasani@utexas.edu>
2021-05-06 13:08:26 -0500

    Home page uses a template

--- KinConnections/app.py
+++ KinConnections/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/static")
 
+# the home/main page
 @app.route('/')
-def main():
-    return render_template('kc_template.html')
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000)
+    app.run(debug=True, host='localhost', port=5000)
