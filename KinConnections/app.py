def1d8fdd08d4de4cf0687dc8ef22518ad83259c
Shanil Jasani <shaniljasani@utexas.edu>
2021-06-07 08:59:57 -0700

    + connections_bp

--- KinConnections/app.py
+++ KinConnections/app.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask, render_template, session
 from auth.auth import auth_bp
+from connections.connections import connections_bp
 from dotenv import load_dotenv
 
 load_dotenv(dotenv_path="../.env")
@@ -9,6 +10,7 @@ load_dotenv(dotenv_path="../.env")
 app = Flask(__name__, static_url_path="/static")
 app.secret_key = os.getenv("APP_SECRET")
 app.register_blueprint(auth_bp)
+app.register_blueprint(connections_bp)
 
 # redirect on trailing slashes
 @app.before_request
@@ -27,34 +29,6 @@ def index():
 @app.route('/home')
 def home():
     return render_template("home.html")
-
-
-newDemoProfile = {
-    "first_name" : "Ali Muhammad",
-    "last_name" : "Joint-LastName",
-    "email" : "ceo@kc.com",
-    "password" : "demopass",
-    "dob" : "37257",
-    "country_origin" : "United States",
-    "region_current" : "United States",
-    "location_current" : "Bay Area, CA",
-    "gender" : "male",
-    "languages" : ["English", "Hindi/Urdu"],
-    "acknowledgement" : "TRUE",
-    "user_type" : "Connector",
-
-    "title" : "CEO of OneTrust | Educator | Teacher | Strategist",
-    "professional_category" : ["Business", "Technology"],
-    "education" : {"School0Name" : "UT Austin", "School0Major" : "ECE"},
-    "linkedin" : "https://linkedin.com/in/kabir",
-    "bio" : "Kabir is the Founder, President, and CEO of OneTrust. In under five years, Kabir has grown the company into the #1 fastest growing company on the Inc. 500 and category-defining enterprise technology platform to operationalize trust. According to TCV, OneTrust is the fastest growing enterprise software company in history. OneTrust has largely pioneered the trust technology market, has been awarded 150 patents, and acquired 7 companies along the way.",
-    "images" : ["/static/img/users/shanil.jpg"],
-    "approved" : "TRUE",
-}
-
-@app.route('/connector/<connector_name>')
-def connector_profile(connector_name):
-    return render_template("connector.html", connector=newDemoProfile)
 
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port=5000)
---
+++ KinConnections/connections/connections.py
@@ -0,0 +1,102 @@
+
+from flask import Blueprint, redirect, render_template, request, session
+
+connections_bp = Blueprint('connections_bp', __name__)
+
+
+
+
+newDemoProfile = {
+    "first_name" : "Ali Muhammad",
+    "last_name" : "Joint-LastName",
+    "email" : "ceo@kc.com",
+    "password" : "demopass",
+    "dob" : "37257",
+    "country_origin" : "United States",
+    "region_current" : "United States",
+    "location_current" : "Bay Area, CA",
+    "gender" : "male",
+    "languages" : ["English", "Hindi/Urdu"],
+    "acknowledgement" : "TRUE",
+    "user_type" : "Connector",
+
+    "title" : "CEO of OneTrust | Educator | Teacher | Strategist",
+    "professional_category" : ["Business", "Technology"],
+    "education" : {"School0Name" : "UT Austin", "School0Major" : "ECE"},
+    "linkedin" : "https://linkedin.com/in/kabir",
+    "bio" : "Kabir is the Founder, President, and CEO of OneTrust. In under five years, Kabir has grown the company into the #1 fastest growing company on the Inc. 500 and category-defining enterprise technology platform to operationalize trust. According to TCV, OneTrust is the fastest growing enterprise software company in history. OneTrust has largely pioneered the trust technology market, has been awarded 150 patents, and acquired 7 companies along the way.",
+    "images" : ["/static/img/users/shanil.jpg"],
+    "approved" : "TRUE",
+}
+
+
+newDemoProfile = {
+    "first_name" : "Ali Muhammad",
+    "last_name" : "Joint-LastName",
+    "email" : "ceo@kc.com",
+    "password" : "demopass",
+    "dob" : "37257",
+    "country_origin" : "United States",
+    "region_current" : "United States",
+    "location_current" : "Bay Area, CA",
+    "gender" : "male",
+    "languages" : ["English", "Hindi/Urdu"],
+    "acknowledgement" : "TRUE",
+    "user_type" : "Connector",
+
+    "title" : "CEO of OneTrust | Educator | Teacher | Strategist",
+    "professional_category" : ["Business", "Technology"],
+    "education" : {"School0Name" : "UT Austin", "School0Major" : "ECE"},
+    "linkedin" : "https://linkedin.com/in/kabir",
+    "bio" : "Kabir is the Founder, President, and CEO of OneTrust. In under five years, Kabir has grown the company into the #1 fastest growing company on the Inc. 500 and category-defining enterprise technology platform to operationalize trust. According to TCV, OneTrust is the fastest growing enterprise software company in history. OneTrust has largely pioneered the trust technology market, has been awarded 150 patents, and acquired 7 companies along the way.",
+    "images" : ["/static/img/users/shanil.jpg"],
+    "approved" : "TRUE"
+}
+
+
+newDemoProfile2 = {
+    "first_name" : "Kabir",
+    "last_name" : "Barday",
+    "email" : "ceo@kc.com",
+    "password" : "demopass",
+    "dob" : "37257",
+    "country_origin" : "USA",
+    "region_current" : "USA",
+    "location_current" : "Bay Area, CA",
+    "gender" : "male",
+    "languages" : ["English", "Hindi/Urdu"],
+    "acknowledgement" : "TRUE",
+    "user_type" : "Connector",
+
+    "title" : "CEO of OneTrust",
+    "professional_category" : ["Business", "Technology"],
+    "education" : {"School0Name" : "UT Austin", "School0Major" : "ECE"},
+    "linkedin" : "https://linkedin.com/in/kabir",
+    "bio" : "hi my name is pickles",
+    "images" : ["/static/img/team-2.jpg", ""],
+    "approved" : "TRUE"
+}
+
+profiles = [newDemoProfile, newDemoProfile2]
+
+
+
+@connections_bp.route('/connectors/<uid>')
+def connector_profile_uid(uid):
+
+    try:
+        currentConnector = profiles[int(uid)]
+    except (IndexError, ValueError) as e:
+        currentConnector = None
+        return render_template("connector.html", connector=currentConnector)
+
+    redirect_string = "/connectors/" + uid + "/" + currentConnector['last_name'] + "/" + currentConnector['first_name'] 
+    return redirect(redirect_string)
+
+@connections_bp.route('/connectors/<uid>/<last_name>/<first_name>')
+def connector_profile_uid_ln_fn(uid, last_name, first_name):
+    try:
+        currentConnector = profiles[int(uid)]
+    except IndexError:
+        currentConnector = None
+    return render_template("connector.html", connector=currentConnector)
