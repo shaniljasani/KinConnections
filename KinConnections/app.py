8e20b6cd04c892c8f37d19daf4fafd0a5491a3b5
Shanil Jasani <shaniljasani@utexas.edu>
2021-08-07 17:56:11 -0700

    Refactor auth/db/signup/login

--- KinConnections/app.py
+++ KinConnections/app.py
@@ -1,14 +1,16 @@
+# app.py - KinConnections
+# Serves as main driver for KinConnections platform
+
 import os
 
-from flask import Flask, render_template, session
-from auth.auth import auth_bp
+from flask import Flask, render_template, session, request, redirect, url_for
 from dotenv import load_dotenv
+from db_wrapper import *
 
 load_dotenv(dotenv_path="../.env")
 
 app = Flask(__name__, static_url_path="/static")
 app.secret_key = os.getenv("APP_SECRET")
-app.register_blueprint(auth_bp)
 
 # redirect on trailing slashes
 @app.before_request
@@ -19,42 +21,108 @@ def clear_trailing():
     if rp != '/' and rp.endswith('/'):
         return redirect(rp[:-1])
 
-# the home/main page
+# the index public page
 @app.route('/')
 def index():
     return render_template("index.html")
 
+# dashboard view for logged in users
 @app.route('/home')
 def home():
-    return render_template("home.html")
+    if session.get('email', None):
+        return render_template("home.html")
+    else:
+        return redirect(url_for('login'))
+
+# user login page
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    # user already logged in
+    if session.get('email', None):
+        return redirect(url_for('home'))
+
+    # login process
+    error = None
+    if request.method == 'POST':
+        email = request.form.get('inputEmail')
+        password = request.form.get('inputPassword')
+        
+        # External Authorization from function
+        error = login_auth(email, password)
+
+        # valid username == no error
+        if(error == None):
+            user_info = login_get_user_info(email)
+            session['email'] = user_info['email']
+            session['name'] = user_info['name'].lower()
+            return redirect(url_for('index'))
+
+    # else return to login page with/without error
+    return render_template('login.html', error=error)
+
+#used to logout a user
+@app.route('/logout')
+def logout():
+    # clear cookie
+    session.clear()
+    # redirect to homepage
+    return redirect(url_for('index'))
+
+# signup - ask user type
+@app.route('/signup')
+def signup():
+    if session.get('email', None):
+        return render_template("home.html")
+    else:
+        return render_template('signup.html', type=None, error=None, success=None)
 
 
-newDemoProfile = {
-    "first_name" : "Ali Muhammad",
-    "last_name" : "Joint-LastName",
-    "email" : "ceo@kc.com",
-    "password" : "demopass",
-    "dob" : "37257",
-    "country_origin" : "United States",
-    "region_current" : "United States",
-    "location_current" : "Bay Area, CA",
-    "gender" : "male",
-    "languages" : ["English", "Hindi/Urdu"],
-    "acknowledgement" : "TRUE",
-    "user_type" : "Connector",
+# signup - connector
+@app.route('/signup/connector')
+def signup_connector():
+    # continue with signup process
+    error = None
+    success = None
+    return render_template('signup.html', type="Connector", error=error, success=success)
 
-    "title" : "CEO of OneTrust | Educator | Teacher | Strategist",
-    "professional_category" : ["Business", "Technology"],
-    "education" : {"School0Name" : "UT Austin", "School0Major" : "ECE"},
-    "linkedin" : "https://linkedin.com/in/kabir",
-    "bio" : "Kabir is the Founder, President, and CEO of OneTrust. In under five years, Kabir has grown the company into the #1 fastest growing company on the Inc. 500 and category-defining enterprise technology platform to operationalize trust. According to TCV, OneTrust is the fastest growing enterprise software company in history. OneTrust has largely pioneered the trust technology market, has been awarded 150 patents, and acquired 7 companies along the way.",
-    "images" : ["/static/img/users/shanil.jpg"],
-    "approved" : "TRUE",
-}
+
+
+# signup - connectee
+@app.route('/signup/connectee', methods=('GET', 'POST'))
+def signup_connectee():
+    # user already logged in
+    if (session.get('email', None)):
+        return redirect(url_for('home'))
+    
+    # continue with signup process
+    error = None
+    success = None
+    if request.method == 'POST':
+        form_entries = {}
+        form_entries['email'] = request.form.get('input_email')
+        form_entries['password'] = request.form.get('input_password')
+        form_entries['first_name'] = request.form.get('input_first_name')
+        form_entries['last_name'] = request.form.get('input_last_name')
+        form_entries['dob'] = request.form.get('input_dob')
+        form_entries['nationality'] = request.form.get('input_nationality')
+        form_entries['region_current'] = request.form.get('input_region_current')
+        form_entries['gender'] = request.form.get('input_gender')
+        form_entries['languages'] = request.form.getlist('language')
+        form_entries['attended_ge'] = request.form.get('attended_ge')
+        form_entries['ge_camps'] = request.form.get('input_ge_camp')
+        form_entries['is_ismaili'] = request.form.get('input_is_ismaili')
+        
+        success, error = signup_new_connectee(form_entries)
+
+        return render_template('signup.html', type="Connectee", error=error, success=success)    
+        
+    return render_template('signup.html', type="Connectee", error=error, success=success)
+
 
 @app.route('/connector/<connector_name>')
 def connector_profile(connector_name):
-    return render_template("connector.html", connector=newDemoProfile)
+    return render_template("connector.html", connector=get_connector_by_name(connector_name))
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port=5000)
