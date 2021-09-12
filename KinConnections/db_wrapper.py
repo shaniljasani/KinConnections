dc52fa9436f7b3576828f111d2928542373402fe
Ata Gowani <atagowani@gmail.com>
2021-09-12 00:26:31 -0400

    Clean up unused code

--- KinConnections/db_wrapper.py
+++ KinConnections/db_wrapper.py
@@ -61,53 +61,6 @@ def signup_new_connectee(form_entries):
 #       CONNECTIONS FUNCTIONS
 # --------------------------------------------------------
 
-newDemoProfile = {
-        "id": 0,
-        "first_name" : "Ali",
-        "last_name" : "Kassim Longnasdfsdfsdfsdfs",
-        "email" : "ceo@kc.com",
-        "password" : "demopass",
-        "dob" : "37257",
-        "country_origin" : "United States",
-        "region_current" : "United States",
-        "location_current" : "Bay Area, CA",
-        "gender" : "male",
-        "languages" : ["English", "Hindi/Urdu"],
-        "acknowledgement" : "TRUE",
-        "user_type" : "Connector",
-
-        "title" : "CEO of OneCompany | Educator | Teacher | Strategist",
-        "professional_category" : ["Business", "Engineering"],
-        "education" : {"School0Name" : "UT Austin", "School0Major" : "ECE"},
-        "linkedin" : "https://linkedin.com/in/kabir",
-        "bio" : "Kabir is the Founder, President, and CEO of OneTrust. In under five years, Kabir has grown the company into the #1 fastest growing company on the Inc. 500 and category-defining enterprise technology platform to operationalize trust. According to TCV, OneTrust is the fastest growing enterprise software company in history. OneTrust has largely pioneered the trust technology market, has been awarded 150 patents, and acquired 7 companies along the way.",
-        "images" : ["/static/img/users/shanil.jpg"],
-        "approved" : "TRUE",
-    }
-newDemoProfile1 = {
-        "id": 0,
-        "first_name" : "Ali",
-        "last_name" : "Kassim Longnasdfsdfsdfsdfs",
-        "email" : "ceo@kc.com",
-        "password" : "demopass",
-        "dob" : "37257",
-        "country_origin" : "United States",
-        "region_current" : "United States",
-        "location_current" : "Bay Area, CA",
-        "gender" : "male",
-        "languages" : ["English", "Hindi/Urdu"],
-        "acknowledgement" : "TRUE",
-        "user_type" : "Connector",
-
-        "title" : "CEO of OneCompany | Educator | Teacher | Strategist",
-        "professional_category" : ["Arts & Media", "Engineering"],
-        "education" : {"School0Name" : "UT Austin", "School0Major" : "ECE"},
-        "linkedin" : "https://linkedin.com/in/kabir",
-        "bio" : "Kabir is the Founder, President, and CEO of OneTrust. In under five years, Kabir has grown the company into the #1 fastest growing company on the Inc. 500 and category-defining enterprise technology platform to operationalize trust. According to TCV, OneTrust is the fastest growing enterprise software company in history. OneTrust has largely pioneered the trust technology market, has been awarded 150 patents, and acquired 7 companies along the way.",
-        "images" : ["/static/img/users/shanil.jpg"],
-        "approved" : "TRUE",
-    }
-
 def get_all_connectors():
     all =  [ connector['fields'] for connector in airtable_connectors.get_all() ]
     all_approved = []
