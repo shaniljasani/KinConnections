deff8a90ddcb95a2d63dbe230c20f59abf1007cd
Ata Gowani <atagowani@gmail.com>
2021-11-11 13:24:50 -0600

    Fix formatting

--- KinConnections/resources/connector.py
+++ KinConnections/resources/connector.py
@@ -4,21 +4,24 @@ from flask import Blueprint, Response, request, render_template, session, redire
 from flask_restful import Resource
 from .db_wrapper import db_wrapper
 
+
 class Connectors(Resource):
     def get(self):
         if not session.get('email', None):
             return redirect("/login?error=notSignedIn&next=connections")
-        
+
         all_connectors = db_wrapper.get_all_connectors()
         return Response(render_template("connectors.html", connectors=all_connectors), mimetype="text/html")
+
 
 class ConnectorsApi(Resource):
     def get(self):
         if not session.get('email', None):
             return 'Unauthorized Access', 401
-        
+
         all_connectors = json.dumps(db_wrapper.get_all_connectors())
         return Response(all_connectors, mimetype="application/json", status=200)
+
 
 class Connector(Resource):
     def get(self, id):
--- KinConnections/resources/routes.py
+++ KinConnections/resources/routes.py
@@ -2,13 +2,14 @@ from resources.connector import Connector, Connectors, ConnectorsApi
 from .auth import Login, Logout, Signup, SignupConnectee
 from .email_wrapper import Email
 
-def initialize_routes(api):
- api.add_resource(Login, '/login')
- api.add_resource(Logout, '/logout')
- api.add_resource(Signup, '/signup')
- api.add_resource(SignupConnectee, '/signup/connectee')
- api.add_resource(Connectors, '/connectors')
- api.add_resource(Connector, '/connector/<id>')
 
- api.add_resource(ConnectorsApi, '/api/connectors')
- api.add_resource(Email, '/api/send_email/<id>')
+def initialize_routes(api):
+    api.add_resource(Login, '/login')
+    api.add_resource(Logout, '/logout')
+    api.add_resource(Signup, '/signup')
+    api.add_resource(SignupConnectee, '/signup/connectee')
+    api.add_resource(Connectors, '/connectors')
+    api.add_resource(Connector, '/connector/<id>')
+
+    api.add_resource(ConnectorsApi, '/api/connectors')
+    api.add_resource(Email, '/api/send_email/<id>')
